913f0eb648c94e6fd06dff351ef3b1724d9fc23a
sam4work <sam4work10@gmail.com>
2024-03-03 23:12:01 +0000

    Update settings.py

--- notes/settings.py
+++ notes/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django_notes",
-        "USER": "postgres",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
